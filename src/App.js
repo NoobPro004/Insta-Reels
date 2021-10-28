9ee91f74945e8c67bbd7648023975f247536f602
NoobPro004 <himanshugarg738@gmail.com>
2021-10-29 00:03:14 +0530

    added upto Login

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Signup from "./Components/Signup"
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter,Switch,Route } from 'react-router-dom';
+import Login from './Components/Login'
 function App() {
   return (
     <BrowserRouter>
-    <Signup/>
+    <Route path="/login" component={Login}/>
+    <Route path="/signup" component={Signup}/>
     </BrowserRouter>
   );
 }
