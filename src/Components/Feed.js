9d6d3f2f9d3cee9ef2d4517237ded159464acd2f
NoobPro004 <himanshugarg738@gmail.com>
2021-10-29 15:47:06 +0530

    added upto Upload feature

--- src/Components/Feed.js
+++ src/Components/Feed.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React,{useEffect,useState,useContext} from 'react'
+import {AuthContext} from '../Context/AuthContext'
+import { database } from '../firebase'
+
+import UploadFile from './UploadFile'
 
 function Feed() {
+
+    const {user,logout} =useContext(AuthContext);
+    const [userData,setUserData] = useState('')
+    useEffect(()=>{
+        const unsub = database.users.doc(user.uid).onSnapshot((snapshot)=>{
+            setUserData(snapshot.data())
+        })
+        return ()=> {unsub()}
+    },[user])
+
     return (
-        <div>
+        <div style={{display:'flex',justifyContent:'center',alignItems:'center',flexDirection:'column'}}>
+            <div className="comp" style={{width:'50%'}}>
             <h1>Welcome to feed</h1>
+            <button onClick={logout}>Log Out</button>
+            </div>
+            <UploadFile user={userData}></UploadFile>
         </div>
     )
 }
